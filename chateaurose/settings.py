556920d19187a536e1fd1fffc15a4d8b75c40300
jafty <japhet.situmonana@gmail.com>
2025-12-21 04:23:37 +0100

    Secure Django secret key and debug config

--- chateaurose/settings.py
+++ chateaurose/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
+import secrets
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-nj^ep_+l5dur=p3n7u!)1q65-a7k@!9pgzge$^lgggdh!ewjp="
+SECRET_KEY = (
+    os.environ.get("SECRET_KEY")
+    or os.environ.get("DJANGO_SECRET_KEY")
+    or secrets.token_urlsafe(50)
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "").lower() in {"1", "true", "yes", "on"}
 
 ALLOWED_HOSTS = []
 
