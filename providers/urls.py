921f699db4b394fa62731c39f7dd5ddd2037d41c
jafty <japhet.situmonana@gmail.com>
2025-12-29 05:20:59 +0100

    Enhance provider forms and secure provider portal

--- providers/urls.py
+++ providers/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
 
+from django.contrib.auth import views as auth_views
+from django.urls import path
+
 from . import views
 
 app_name = "providers"
 
 urlpatterns = [
     path("", views.index, name="providers_index"),
+    path(
+        "connexion/",
+        auth_views.LoginView.as_view(template_name="providers/login.html"),
+        name="login",
+    ),
+    path(
+        "deconnexion/",
+        auth_views.LogoutView.as_view(next_page="interface:home"),
+        name="logout",
+    ),
 ]
