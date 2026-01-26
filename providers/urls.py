3da60f37469463ac8603ddf14dd8006e9209ac17
jafty <japhet.situmonana@gmail.com>
2026-01-26 14:47:50 +0100

    Add provider password reset flow

--- providers/urls.py
+++ providers/urls.py
@@ -17,6 +17,36 @@ urlpatterns = [
         name="login",
     ),
     path(
+        "mot-de-passe/",
+        auth_views.PasswordResetView.as_view(
+            template_name="providers/password_reset.html",
+            email_template_name="providers/password_reset_email.html",
+            subject_template_name="providers/password_reset_subject.txt",
+        ),
+        name="password_reset",
+    ),
+    path(
+        "mot-de-passe/envoye/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="providers/password_reset_done.html",
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "mot-de-passe/confirmation/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="providers/password_reset_confirm.html",
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "mot-de-passe/termine/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="providers/password_reset_complete.html",
+        ),
+        name="password_reset_complete",
+    ),
+    path(
         "deconnexion/",
         auth_views.LogoutView.as_view(next_page="interface:home"),
         name="logout",
